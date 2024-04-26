15012e8fe3fad0e0e1480f49ac4a73663d9507f8
Aditya bandaru <adityabandaruuu18@gmail.com>
2024-04-26 22:28:46 +0530

    Index and script files added

---
+++ script.php
@@ -0,0 +1,19 @@
+<html>
+
+<body>
+
+Welcome <?php echo $_POST["first-name"]; ?> <br>
+
+Your last name is: <?php echo $_POST["last-name"]; ?> <br>
+
+Your email address is: <?php echo $_POST["email"]; ?> <br>
+
+Your Password is: <?php echo $_POST["password"]; ?> <br>
+
+Your Address is: <?php echo $_POST["address"]; ?> <br>
+
+Your Mobile no is: <?php echo $_POST["mobile"]; ?> <br>
+
+</body>
+
+</html>
